c2c5c11e1f9f8420a6fe293e114387ebdd6d967c
T_GulDan <grayman219@gmail.com>
2019-05-08 00:31:27 +0300

    add flexbox to updating list

--- src/containers/mainpage_updatinglist/updating_list_item.js
+++ src/containers/mainpage_updatinglist/updating_list_item.js
@@ -16,14 +16,17 @@ class UpdatingListItem extends React.Component {
     return (
        
             <li className="updating-list__item">
-                <Link to={this.props.link} >
-                  <span className="updating-list__item_date">
-                      {this.props.date}
-                  </span>
-                  <img className="updating-list__item_preview" src={this.props.imageSrc} alt="articlePreview" />
-                  <span className="updating-list__item_name">
-                      {this.props.name}
-                  </span>
+                <Link to={this.props.link} className="updating-list__item_link">
+
+                    <span className="updating-list__item_date">
+                        {this.props.date}
+                    </span>
+                    <img className="updating-list__item_preview" src={this.props.imageSrc} alt="articlePreview" />
+                    <span className="updating-list__item_name">
+                        {this.props.name}
+                    </span>
+     
+                  
                 </Link>
             </li>
         
